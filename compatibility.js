7d4f9ca34310ab89fc449dd2e032814dbaaef92c
Tiny <zjutiny@gmail.com>
2015-09-19 21:34:01 +0800

    add full screen scroll plugin

--- compatibility.js
+++ compatibility.js
@@ -11,6 +11,7 @@ EventUtil = {
             obj['on' + event] = handler;
         }
     },
+
     removeHandler: function (obj, event, handler) {
         if (obj.removeEventListener) {
             obj.removeEventListener(event, handler, false);
@@ -19,5 +20,51 @@ EventUtil = {
         } else {
             obj['on' + event] = null;
         }
+    },
+
+    getEvent: function (event) {
+        return event ? event : window.event;
+    },
+
+    getTarget: function (event) {
+        return event.target || event.srcElement;
+    }
+
+    preventDefault: function (event) {
+        if(event.preventDefault){
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+
+    stopPropagation: function (event) {
+        if(event.stopPropagation){
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    }
+    //Example
+    // node.onclick = function (event) {
+    //     event = EventUtil.getEvent(event);
+    //     var target = EventUtil.getTarget(event);
+    // };
+};
+
+
+
+//get the window size
+var getClientSize = function(){
+    if(document.compatMode == "BackCompact"){
+        return {
+            width: document.body.clientWidth,
+            height: document.body.clientHeight
+        };
+    } else {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        };
     }
 };
